86edf50beb2483921f087d8a3b63d936bd5e0307
sjgoebel <sgoebel@gmail.com>
2019-03-01 15:52:23 -0600

    update make

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc oh-my-zsh gitconfig emacs.d"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc oh-my-zsh gitconfig emacs.d tmux.conf tmux.conf.local"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -24,8 +24,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+#    echo "Moving any existing dotfiles from ~ to $olddir"
+#    mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
